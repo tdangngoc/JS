cfd9d769584b438618704805d547a95be3055b48
tdangngoc <toan94300@gmail.com>
2021-08-25 18:09:52 +0200

    Add dom.js + modify index.html files

---
+++ dom.js
@@ -0,0 +1,16 @@
+const ul = document.querySelector('.items');
+
+//ul.remove();
+//ul.lastElementChild.remove();
+//ul.firstElementChild.textContent = "Hello";
+
+const btn = document.querySelector('.btn');
+
+//btn.style.background = 'red';
+
+btn.addEventListener('mouseout', (e) => {
+    e.preventDefault();
+    document.querySelector('#my-form').style.background = "#ccc";
+    document.querySelector('body').classList.add('bg-dark');
+    document.querySelector('.items').lastElementChild.innerHTML = '<h1>Hello</h1>';
+});
