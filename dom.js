400ccf36780c2e863f7108f089c377cf88649d9d
tdangngoc <toan94300@gmail.com>
2021-08-25 18:32:41 +0200

    Adding little code to test DOM events

--- dom.js
+++ dom.js
@@ -8,9 +8,37 @@ const btn = document.querySelector('.btn');
 
 //btn.style.background = 'red';
 
-btn.addEventListener('mouseout', (e) => {
+/* btn.addEventListener('mouseout', (e) => {
     e.preventDefault();
     document.querySelector('#my-form').style.background = "#ccc";
     document.querySelector('body').classList.add('bg-dark');
     document.querySelector('.items').lastElementChild.innerHTML = '<h1>Hello</h1>';
-});
+}); */
+
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e){
+    e.preventDefault();
+
+    if(nameInput.value === '' || emailInput.value === ''){
+        msg.classList.add('error')
+        msg.innerHTML = 'Please enter all fields'
+
+        setTimeout(() => msg.remove(), 3000);
+    } else {
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+
+        userList.appendChild(li);
+
+        //clear the field
+        nameInput.value = '';
+        emailInput.value = '';
+    }
+}
